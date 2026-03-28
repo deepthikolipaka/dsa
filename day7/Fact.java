9566f75f132fcfb5405725aebe3ac4c87ebdfeea
deepthikolipaka <deepthikolipakaaa@gmail.com>
2026-03-29 05:23:34 +0530

    Day 7

---
+++ day7/Fact.java
@@ -0,0 +1,17 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Fact {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        int i,fact=1;
+        for(i=n;i>=1;i--){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of given number:"+fact);
+
+    }
+}
