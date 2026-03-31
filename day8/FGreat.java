634c0843665033d3bf743c41e3c15acaf9c2728a
deepthikolipaka <deepthikolipakaaa@gmail.com>
2026-03-31 05:40:09 +0530

    Day 9

--- day8/FGreat.java
+++ day8/FGreat.java
@@ -1,9 +1,29 @@
 package day8;
 
+import java.util.Scanner;
+
 public class FGreat {
-    public static int main(int a,int b) {
-        if(a>b)
-            
+    public static void main(String []args) {
+            great();
+             great();
+              great();
+    }
+    static int great(){
+        int a,b;
+        Scanner in=new Scanner(System.in);
+        a=in.nextInt();
+        b=in.nextInt();
+        if(a>b){
+            System.out.println(a+"is bigger");
+        }
+        else if(b>a){
+            System.out.println(b+"is greater");
+        }
+        else{
+            System.out.println("Both are equal");
+        }
+        return a;
+
     }
     
 }
---
+++ day9/Greet.java
@@ -0,0 +1,18 @@
+package day9;
+//Greeting by using methods/functions
+
+public class Greet {
+    public static void main(String[] args) 
+    {
+        greeet();
+        greeet();
+        greeet();
+        greeet();
+        greeet();
+    }
+        
+        static void greeet(){
+            System.out.println("Hello , How are you? ");
+        }
+    }
+
