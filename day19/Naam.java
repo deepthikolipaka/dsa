f615dec41e196a7ed45dfec1d4e2ba8b482d8b30
deepthikolipaka <deepthikolipakaaa@gmail.com>
2026-04-12 11:32:25 +0530

    Methods Confusion

---
+++ day19/Naam.java
@@ -0,0 +1,14 @@
+package day19;
+public class Naam {
+    public static void main(String[] args) {
+       String n="Swarupa"; //if none of first and second there the main method variable value will returns
+       //name("Deeee");  //Second preference
+       name(n);
+    }
+     static void name(String n) {
+        //n="Deepthi"; //first prefernece if not have a value then second prefernece fetched
+        System.out.println("Your name is :"+n);
+    
+}
+    
+}
