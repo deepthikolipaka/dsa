c7563a877cfd4a3ea81ef34335c168050e34b5b2
deepthikolipaka <deepthikolipakaaa@gmail.com>
2026-04-14 11:46:50 +0530

    Functions confusion

---
+++ day21/Changevalue.java
@@ -0,0 +1,14 @@
+package day21;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Changevalue{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int [] arr={1,6,8,7};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int []num){
+        num [0]=87;
+    }
+}
